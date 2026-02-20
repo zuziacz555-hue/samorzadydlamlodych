fa3ff4372d5997b0cad16455b8dc66600786d792
zuziacz555-hue <zuziacz555@gmail.com>
2026-02-20 02:44:52 +0100

    Setup Global Admin Access

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 /**
  * Global Configuration for Admin Access
- * Last Updated: 2/20/2026, 2:05:00 AM (Manual Setup)
+ * Last Updated: 20.02.2026, 02:44:52
  */
 window.SITE_CONFIG = {
     auth: {
-        "cipher": "FuYCKsEZMlQbdi3Lo93euh+yjD/oreoSURn/AcyiG5Y6eIjTj4ZWjMXyhZz++X2vatZm1iCtIa0=",
-        "iv": "qd3qPSkic9xoLF2F",
-        "salt": "cbc52dcb-a1d5-4ff9-9795-ab8a7576497e"
-    }
+    "cipher": "uRUHt6EJRTh2Rxcq66k+Rl8DEI6zrW0ZH6Xp2tGXqmUytFaioJRgJJfJf8+8Cr5+xrcZeKkpUzE=",
+    "iv": "+WCJLTByTd/0X3Jr",
+    "salt": "fbeec39c-ef76-499a-aa84-ea33570c1766"
+}
 };
