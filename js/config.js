7fd8db4e16d430aa7ea2236d58b5e954dc96fb4d
zuziacz555-hue <zuziacz555@gmail.com>
2026-02-20 02:02:54 +0100

    Manual Setup: Global Admin Access Config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,11 @@
 /**
  * Global Configuration for Admin Access
- * This file updates automatically when you run the setup in Admin Mode.
+ * Last Updated: 2/20/2026, 2:05:00 AM (Manual Setup)
  */
 window.SITE_CONFIG = {
-    auth: null // Will contain { cipher, iv, salt } after setup
+    auth: {
+        "cipher": "FuYCKsEZMlQbdi3Lo93euh+yjD/oreoSURn/AcyiG5Y6eIjTj4ZWjMXyhZz++X2vatZm1iCtIa0=",
+        "iv": "qd3qPSkic9xoLF2F",
+        "salt": "cbc52dcb-a1d5-4ff9-9795-ab8a7576497e"
+    }
 };
